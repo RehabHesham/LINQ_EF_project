a6b277cda280e770229dc0b0d14ce7a85c39b257
Ro98h <rehabhesham98@yahoo.com>
2022-03-13 12:35:44 +0200

    2nd commit

---
+++ Bills/DBcontext/Company.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bills.DBcontext
+{
+    public class Company
+    {
+        [Key]
+        public int Comp_id { get; set; }
+        [Required]
+        [StringLength(150)]
+        [Index(IsUnique = true)]
+        public string Comp_name { get; set; }
+        [StringLength(500)]
+        public string Comp_notes { get; set; }
+    }
+}
